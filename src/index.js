f8f81070662d05e10ed5259348f7ec4c4e35840c
Ruslan Kim <div.doctor@gmail.com>
2020-07-29 20:38:58 +0300

    [Step-2] add --format option and file paths arguments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const program = () => {
   commander
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
     .parse(process.argv);
 };
 
