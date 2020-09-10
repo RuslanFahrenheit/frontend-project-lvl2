b65176f52b84d0e96840c98f70f57878e2ee1bb3
Ruslan Kim <div.doctor@gmail.com>
2020-09-10 16:13:57 +0300

    refactor code in accordance with the mentor's remarks

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,10 +2,15 @@ import { renderStylishDiff } from './stylish.js';
 import { renderPlainDiff } from './plain.js';
 import { renderJsonDiff } from './json.js';
 
-const formatters = {
-  stylish: renderStylishDiff,
-  plain: renderPlainDiff,
-  json: renderJsonDiff,
+export const render = (diff, format) => {
+  switch (format) {
+    case 'stylish':
+      return renderStylishDiff(diff);
+    case 'plain':
+      return renderPlainDiff(diff);
+    case 'json':
+      return renderJsonDiff(diff, null, 2);
+    default:
+      throw new Error(`Unexpected format ${format}`);
+  }
 };
-
-export const render = (diff, format) => formatters[format](diff);
--- src/parsers.js
+++ src/parsers.js
@@ -2,10 +2,15 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 import { EXTENSION_TYPES } from './constants/extensionTypes.js';
 
-const parsers = {
-  [EXTENSION_TYPES.json]: JSON.parse,
-  [EXTENSION_TYPES.yml]: yaml.safeLoad,
-  [EXTENSION_TYPES.ini]: ini.parse,
+export const parse = (data, format) => {
+  switch (format) {
+    case EXTENSION_TYPES.json:
+      return JSON.parse(data);
+    case EXTENSION_TYPES.yml:
+      return yaml.safeLoad(data);
+    case EXTENSION_TYPES.ini:
+      return ini.parse(data);
+    default:
+      throw new Error(`Unexpected file extension ${format}`);
+  }
 };
-
-export const parse = (data, format) => parsers[format](data);
