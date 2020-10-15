7c6babbeb0b5bed4d33d8bc9adce12d6d956e17c
Ruslan Kim <rkim@justcoded.co>
2020-10-15 16:04:16 +0300

    refactor code in accordance with the mentor's remarks

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,42 +1,39 @@
 import _ from 'lodash';
 import { NODE_TYPES } from '../constants/nodeTypes.js';
 
-const tabSize = 4;
-const makeIndent = (depth, tab = ' ') => tab.repeat(depth);
+const tabSize = 2;
+const indent = (depth, tab = '  ') => tab.repeat(depth);
 
 const stringify = (node, depth) => {
   if (!_.isPlainObject(node)) {
     return node;
   }
 
-  const indent = makeIndent(tabSize * depth);
-  const unindent = makeIndent(tabSize * depth - tabSize);
-
   const result = Object.entries(node).flatMap(
     ([key, value]) => (
-      `\n${indent}${key}: ${stringify(value, depth + 1)}`
+      `\n${indent(tabSize * depth)}${key}: ${stringify(value, depth + 1)}`
     ),
   ).join('');
 
-  return `{${result}\n${unindent}}`;
+  return `{${result}\n${indent(tabSize * depth - tabSize)}}`;
 };
 
-const renderDiff = (diff, depth = 1) => {
-  const indent = makeIndent(tabSize * depth);
-  const halfIndent = makeIndent(tabSize * depth - tabSize / 2);
+const mapping = {
+  [NODE_TYPES.added]: ({ key, value }, depth) => `${indent(tabSize * depth - 1)}+ ${key}: ${stringify(value, depth + 1)}`,
+  [NODE_TYPES.changed]: (data, depth) => (
+    [mapping[NODE_TYPES.removed](data, depth), mapping[NODE_TYPES.added](data, depth)]
+  ),
+  [NODE_TYPES.nested]: ({ key, children }, depth, iter) => `${indent(tabSize * depth)}${key}: {\n${iter(children, depth + 1)}\n${indent(tabSize * depth)}}`,
+  [NODE_TYPES.removed]: ({ key, removedValue }, depth) => `${indent(tabSize * depth - 1)}- ${key}: ${stringify(removedValue, depth + 1)}`,
+  [NODE_TYPES.unchanged]: ({ key, value }, depth) => `${indent(tabSize * depth)}${key}: ${stringify(value, depth + 1)}`,
+};
 
-  const mapping = {
-    [NODE_TYPES.added]: ({ key, value }) => `${halfIndent}+ ${key}: ${stringify(value, depth + 1)}`,
-    [NODE_TYPES.changed]: (data) => (
-      [mapping[NODE_TYPES.removed](data), mapping[NODE_TYPES.added](data)]
-    ),
-    [NODE_TYPES.nested]: ({ key, children }) => `${indent}${key}: {\n${renderDiff(children, depth + 1)}\n${indent}}`,
-    [NODE_TYPES.removed]: ({ key, removedValue }) => `${halfIndent}- ${key}: ${stringify(removedValue, depth + 1)}`,
-    [NODE_TYPES.unchanged]: ({ key, value }) => `${indent}${key}: ${stringify(value, depth + 1)}`,
-  };
+const renderDiff = (diff) => {
+  const iter = (nodes, depth) => (
+    nodes.flatMap(({ type, ...data }) => mapping[type](data, depth, iter)).join('\n')
+  );
 
-  return diff.flatMap(({ type, ...data }) => mapping[type](data))
-    .join('\n');
+  return iter(diff, 1);
 };
 
 export const renderStylishDiff = (diff) => `{\n${renderDiff(diff)}\n}`;
